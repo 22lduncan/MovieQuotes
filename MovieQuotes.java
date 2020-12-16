f36af70fb32172dd0838b0584b4fb396db94c362
22lduncan <73915056+22lduncan@users.noreply.github.com>
2020-12-15 20:09:28 -0500

    Add files via upload

---
+++ MovieQuotes.java
@@ -0,0 +1,42 @@
+/**
+ * Algorithm MovieQuotes
+ *  Replace and fill these lines
+ *  with your algorithm 
+ *  
+ *  
+ *  
+ * End
+ *----------------------------------
+ * @author: Lucas Duncan
+ * @assignment: Lab MovieQuotes
+ * @date started: Dec 15, 2020
+ * @date completed: Dec 15, 2020
+ *
+ * If you received help from somebody else for this Lab list their name below
+ *
+ *
+ * NOTE: You will not lose points for receiving help from somebody, however, you
+ *       will receive a zero if you do not list names and it is discovered your work
+ *       is not original.
+ */
+
+package MovieQuotes;
+
+public class MovieQuotes {
+    public static void main(String[] args) {
+        String baseball = "There's no crying in baseball!";
+        String chocolate = "My mama always said life was like a box of chocolates. You never know what you're going to get.";
+        String treasure = "Badges? We ain't got no badges!  We don't need no badges!  I don't have to show you any stinking badges!";
+
+        //print baseball to lowercase using a method
+        System.out.println(baseball.toLowerCase());
+        //print chocolate from life to the end using a method
+        System.out.println(chocolate.substring(20));
+        //print treasure from first We to the third badges
+        System.out.println(treasure.substring(8,30));
+        //prints
+        
+        
+    } //end of main method, put code above this line
+
+}//end of class MovieQuotes
